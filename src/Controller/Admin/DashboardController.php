7b787d8a447069a65eb2825b356d33817d7814b7
Tatyana Bessarab <nimeri89@gmail.com>
2020-10-20 09:18:25 +0300

    first settings for admin dashboard

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+use App\Entity\Address;
+use App\Entity\Banner;
+use App\Entity\Measure;
+use App\Entity\OrderProducts;
+use App\Entity\Orders;
+use App\Entity\Product;
+use App\Entity\User;
+
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        return $this->redirect($routeBuilder->setController(CategoryCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Sole-pizza');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
+    }
+}
