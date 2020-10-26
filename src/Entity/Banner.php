7a23d53a7b70caac2ea1c464b80dc179ff478235
Tatyana Bessarab <nimeri89@gmail.com>
2020-10-26 10:50:31 +0300

    deleted sluggerinterface from banner

--- src/Entity/Banner.php
+++ src/Entity/Banner.php
@@ -6,7 +6,6 @@ use App\Repository\BannerRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
